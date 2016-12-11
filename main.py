bb092a30c500fdbd0baab2ea4e0ffecb2315b11a
alexmooc2 <alexmooc2@gmail.com>
2016-12-10 20:20:59 -0800

    "second commmit"

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import tone
 import last
 import genius
 
+#second commit yay!
+
 JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
